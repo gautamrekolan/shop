78bf3a38fd0d2d3307756668337a7ab4d481f000
Eric Nordmeyer <ejpres@ericnordmeyer.de>
2012-02-17 14:19:46 +0100

    face lifting homepage

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,18 @@
 module PagesHelper
+  def preview (string, count = 300)
+    # String::shorten (naiv)
+  # Autor: Martin Labuschin
+  # Erstellt am 29.Juli 2007
+  # String wird auf die durch count (optional, standard 30) bestimmte Anzahl von Zeichen gekürzt. Es werden dann ganze Wörter (erkannt durch Leerräume) zurückgegeben. Falls wirklich eine Kürzung duchgeführt wurde, wird ' ...' angehangen.
+  # BEMERKUNG: Es wird nur Plaintext erwartet
+    if string.length >= count 
+      shortened = string[0, count]
+      splitted = shortened.split(/ /)
+      words = splitted.length
+      splitted[0, words-1].join(" ") + ' ...'
+    else 
+      string
+    end
+  end
+
 end
